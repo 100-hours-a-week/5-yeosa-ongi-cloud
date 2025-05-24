2e75a49774be88ceb00c84c4c84126aeadb4f80d
SeoYoung <seoyoung7623@naver.com>
2025-05-25 00:47:32 +0900

    fix: v1 prod main 수정

--- v1-single-instance/env/prod/main.tf
+++ v1-single-instance/env/prod/main.tf
@@ -5,46 +5,65 @@ provider "google" {
 }
 
 module "vpc" {
-  source        = "../../../modules/gcp/vpc"
-  vpc_name      = var.vpc_name
-  subnet_name   = var.subnet_name
-  region        = var.region
-  ip_cidr_range = var.ip_cidr_range
+  source        = "../../modules/gcp/vpc"
+  vpc_name      = "prod-ongi-vpc"
+  subnet_name   = "prod-ongi-subnet"
+  ip_cidr_range = "10.10.20.0/24"
 }
 
 # 웹 인스턴스
 module "web_instance" {
-  source          = "../../../modules/gcp/instance"
-  instance_name   = var.web_instance_name
+  source          = "../../modules/gcp/instance"
+  instance_name   = "prod-ongi-web"
   zone            = var.zone
-  machine_type    = var.web_machine_type
-  boot_image      = var.web_boot_image
+  machine_type    = "e2-medium"
+  boot_image      = "ubuntu-os-cloud/ubuntu-2204-lts"
+  boot_disk_size  = 20
+  boot_disk_type  = "pd-standard"
   vpc             = module.vpc.vpc_name
   subnet          = module.vpc.subnet_self_link
-  tags            = var.web_tags
-  startup_script  = var.web_startup_script
-  application_yml = var.web_application_yml
+  tags            = ["prod-web"]
+  assign_external_ip = true
+  use_static_ip  = true
 }
 
-# AI 인스턴스
 module "ai_instance" {
-  source          = "../../../modules/gcp/instance"
-  instance_name   = var.ai_instance_name
+  source          = "../../modules/gcp/instance"
+  instance_name   = "prod-ongi-ai"
   zone            = var.zone
-  machine_type    = var.ai_machine_type
-  boot_image      = var.ai_boot_image
+  machine_type    = "n2d-standard-4"
+  boot_image      = "ubuntu-os-cloud/ubuntu-2204-lts"
+  boot_disk_size  = 40
+  boot_disk_type  = "pd-standard"
   vpc             = module.vpc.vpc_name
   subnet          = module.vpc.subnet_self_link
-  tags            = var.ai_tags
-  startup_script  = var.ai_startup_script
-  application_yml = var.ai_application_yml
+  tags            = ["prod-ai"]
+  assign_external_ip = false
 }
 
-
-module "firewall" {
-  source        = "../../../modules/gcp/firewall"
-  firewall_name = var.firewall_name
+module "firewall_allow_ai" {
+  source        = "../../modules/gcp/firewall"
+  firewall_name = "prod-allow-ai"
   vpc           = module.vpc.vpc_name
-  target_tags   = var.target_tags
-  allowed_ports  = var.allowed_ports
+  allowed_ports = ["8000"]
+  source_tags   = ["prod-web"]
+  target_tags   = ["prod-ai"]
+}
+
+module "firewall_allow_web" {
+  source        = "../../modules/gcp/firewall"
+  firewall_name =  "prod-allow-web"
+  vpc           = module.vpc.vpc_name
+  allowed_ports = ["8080"]
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["prod-web"]
+}
+
+module "firewall_allow_http_ssh" {
+  source        = "../../modules/gcp/firewall"
+  firewall_name = "prod-allow-http-ssh"
+  vpc           = module.vpc.vpc_name
+  allowed_ports = ["22", "80", "443"]
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["prod-web", "prod-ai"]
 }
